2b780f08d9cd99ce97db17103d5e4b8ecac2a44e
Sderftg2 <231FA04F37@gmail.com>
2025-04-02 18:15:43 +0530

    Update rb.py

--- rb.py
+++ rb.py
@@ -3,7 +3,7 @@ import datetime
 from telegram import Update
 from telegram.ext import Application, CommandHandler, CallbackContext
 
-TELEGRAM_BOT_TOKEN = '7916293459:AAF6_xPjasVbWzPGPr-fm0FunHH7SJYtbLk'
+TELEGRAM_BOT_TOKEN = '7110991582:AAHWArUkUAtPpWykh74Y85g-Tyx4M6iUllE'
 ADMIN_USER_ID = 5142603617
 USERS_FILE = 'users.txt'
 LOG_FILE = 'log.txt'
